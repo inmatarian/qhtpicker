f534fce93bef4ffd8a2198e8aa401c0166f1b32e
Inny <inmatarian@gmail.com>
2010-08-12 21:22:11 -0400

    add handler launching support

--- qhtpicker/handler.py
+++ qhtpicker/handler.py
@@ -1,18 +1,28 @@
 
-import os, sys, logging
+import os, sys, logging, fnmatch, subprocess
 from logging import debug, info, warning, error, critical
 
 # --------------------------------------
-
-def getFileExtention( filename ):
-    return filename.split(".")[-1]
 
 class Handler(object):
     def __init__(self, config):
         self.config = config
 
+    def launch(self, filename, launcher):
+        info("Launching %s" % filename)
+        cmdline = launcher.replace("$FILE", filename)
+        debug("Command Line: %s" % cmdline)
+        subprocess.Popen(cmdline, shell=True)
+        return
+
     def handle(self, filename):
         debug("Handling %s" % filename)
+        for i in self.config.handlers:
+            glob = i[0]
+            launcher = i[1]
+            if fnmatch.fnmatch(filename, glob):
+                self.launch(filename, launcher)
+        return
 
 # --------------------------------------
 
